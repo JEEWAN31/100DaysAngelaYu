2d11cbb704084993933a22a2f887756e26d86838
Jeewan <jeewanjj02@gmail.com>
2022-12-04 17:13:24 +0530

    Day1 completed

---
+++ Day1/main.py
@@ -0,0 +1,9 @@
+
+print("Hello " + input("What is your name?"))
+
+print("Here is the end of some basics about Input anf Print statements in python")
+
+print("Welcome to the Band Name Generator.")
+street = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print("Your band name could be " + street + " " + pet)
